f5aba933615ae981fbe0065f29ff498feea79337
Elvin Garcia <elvingarcia19@gmail.com>
2023-07-19 20:05:14 -0400

    finished dogModel

---
+++ models/DogModel.js
@@ -0,0 +1,38 @@
+const mongooses = require("mongoose")
+
+const dogSchema = new mongoose.schema({
+
+    name: {
+        type: String,
+        required: true,
+      },
+      age: {
+        type: Number,
+        required: true,
+      },
+      favoriteFood: {
+        type: String,
+        required: true,
+      },
+      funFact: {
+        type: String,
+        required: true,
+      },
+
+      image: {
+        type: String,  // we dont want to store media in our databse. Cloaudanery stores the image on their server and give us a url
+        require: true,
+      },
+      cloudinaryId: {
+        type: String,
+        require: true,
+      },
+
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      }
+
+})
+
+module.exports = mongoose.model("Dog", dogSchema)
