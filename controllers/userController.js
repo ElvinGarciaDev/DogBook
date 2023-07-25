ffe334dd09e0db622beb239eb1ad322ad28ea19a
Elvin Garcia <elvingarcia19@gmail.com>
2023-07-25 15:37:41 -0400

    userROutes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,48 @@
 const passport = require("passport")
 const User = require("../models/userModel")
+
+module.exports = {
+
+    loginPage: async (req, res) => {
+        res.render("login")
+    },
+
+    registerPage: async (req, res) => {
+        res.render("resister")
+    },
+
+    
+    loginUser: passport.authorize("local", {
+        successRedirect: '/', // If login is successful send them to the homepage
+        failureRedirect: "/login" // if not able to login send them back to login page,
+        
+    }),
+
+    resisterUser: async (req, res) => {
+        try {
+
+            // Take the req.body that should hold the usernane and password and assign them to these variables
+            const {username, password} = req.body
+
+            const user = new User({username})
+            await User.register(user,password)
+
+            passport.authenticate('local')(req, res, function() {
+                res.redirect('/')
+            })
+
+            
+        } catch (error) {
+            console.log(error)
+            res.redirect("/register")
+        }
+    },
+
+    logoutUser: (req, res) => {
+        res.logout(function(err) {
+            if(err) {return next(err)}
+            res.redirect('/')
+        })
+    }
+
+}
